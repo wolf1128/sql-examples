9173f8cd1f21c7687ac6e99330109d8f63d75498
Salar Farahmand <professor.dev.run@gmail.com>
2024-11-14 01:03:28 +0100

    Added basics.lvl-0

---
+++ basics/lvl-0.sql
@@ -0,0 +1,47 @@
+/*
+* Covered Examples:
+    - EX#1 | SELECT
+    - EX#2 | SELECT
+    - EX#3 | SELECT with COUNT
+    - EX#4 | SELECT with WHERE
+    - EX#5 | SELECT with WHERE using AND operator
+    - EX#6 | SELECT with WHERE using LIKE operator
+    - EX#7 | SELECT with WHERE using BETWEEN operator
+    - EX#8 | SELECT with aliases
+*/
+
+-- Ex#1
+SELECT *
+FROM employees;
+
+-- Ex#2
+SELECT id, name, salary
+FROM employees;
+
+-- EX#3
+SELECT COUNT(*)
+FROM employees;
+
+-- EX#4
+SELECT *
+FROM employees
+WHERE salary > 10000;
+
+-- EX#5
+SELECT *
+FROM employees
+WHERE salary > 10000 and department_id = 1 ;
+
+-- EX#6
+SELECT *
+FROM employees
+WHERE name LIKE 'A%';
+
+-- EX#7
+SELECT *
+FROM employees
+WHERE salary BETWEEN 70000 AND 100000;
+
+-- EX#8
+SELECT id AS EmployeeID, name AS EmployeeName, salary AS EmployeeSalary
+FROM employees;
